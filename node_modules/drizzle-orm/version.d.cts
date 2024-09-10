44e140f56b66823941a47b3afd0af1a9b57ca16e
Mateus Gurgel <mateusgurgel15@gmail.com>
2024-09-10 15:17:45 -0300

    chore: remove node modules from git

--- node_modules/drizzle-orm/version.d.cts
+++
@@ -1,5 +0,0 @@
-var version = "0.33.0";
-
-declare const compatibilityVersion = 7;
-
-export { compatibilityVersion, version as npmVersion };
--- node_modules/drizzle-orm/version.js
+++
@@ -1,9 +0,0 @@
-// package.json
-var version = "0.33.0";
-
-// src/version.ts
-var compatibilityVersion = 7;
-export {
-  compatibilityVersion,
-  version as npmVersion
-};
